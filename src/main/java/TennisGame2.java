e46314ef7f2739545b7f73faae36f397f0e32284
Anthika Jirattananon <anthikajir@mathregit.group>
2020-08-04 14:53:01 +0700

    second refector

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -7,79 +7,47 @@ public class TennisGame2
     private final String player1Name;
     private final String player2Name;
 
+
+
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
-    public String point(int point){
-        switch (point) {
-            case 0 :
-                return "Love";
-            case 1 :
-                return "Fifteen";
-            case 2 :
-                return "Thirty";
-            case 3 :
-                return "Forty";
-
-        }
-        return null;
-    }
-
 
     public String getScore(){
-        String player1Result = point(player1Point);
-        String player2Result = point(player2Point);
-        String score = "";
-        if (player1Point == player2Point && player1Point < 4)
-        {
-            score = point(player1Point);
-            score += "-All";
-        }
-        if (player1Point == player2Point && player1Point >=3)
-            score = "Deuce";
+        String[] textScores = {"Love","Fifteen","Thirty","Forty"};
 
-        if (player1Point > 0 && player2Point ==0)
-        {
-            score = player1Result + "-" + player2Result;
+        // Winner
+        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
+            return "Win for " + player1Name;
         }
-        if (player2Point > 0 && player1Point ==0)
-        {
-
-            score = player1Result + "-" + player2Result;
+        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
+            return "Win for " + player2Name;
         }
 
-        if (player1Point > player2Point && player1Point < 4)
-        {
-
-            score = player1Result + "-" + player2Result;
+        // Advantage
+        if (player1Point > player2Point && player2Point >= 3) {
+            return "Advantage " + player1Name;
         }
-        if (player2Point > player1Point && player2Point < 4)
-        {
-            score = player1Result + "-" + player2Result;
+        if (player2Point > player1Point && player1Point >= 3) {
+            return "Advantage " + player2Name;
         }
 
-        if (player1Point > player2Point && player2Point >= 3)
-        {
-            score = "Advantage player1";
+        // Deuce
+        boolean isDeuce = player1Point == player2Point && player1Point >= 3;
+        if (isDeuce) {
+            return "Deuce";
         }
 
-        if (player2Point > player1Point && player1Point >= 3)
-        {
-            score = "Advantage player2";
+        // Equal
+        boolean pointEqualbutNotdeuce = player1Point == player2Point && player1Point < 3;
+        if (pointEqualbutNotdeuce) {
+            return textScores[player1Point] + "-All";
         }
 
-        if (player1Point >=4 && player2Point >=0 && (player1Point - player2Point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (player2Point >=4 && player1Point >=0 && (player2Point - player1Point)>=2)
-        {
-            score = "Win for player2";
-        }
-        return score;
+        // Normal cases
+        return textScores[player1Point] + "-" + textScores[player2Point];
     }
-
 
 
     public void player1Score(){
